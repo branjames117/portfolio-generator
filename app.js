8a0607d0ef19668ffa5322de96446fcf7635cc7e
Brandon Hoskins <branjames117@gmail.com>
2021-11-28 12:02:54 -0600

    generating html file with fs module

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = (profileDataArr) => {
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+const [username, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(username, github), (err) => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
