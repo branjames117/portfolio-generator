4ac9c908117e869dc09e316bcae3b61b59683221
Brandon Hoskins <branjames117@gmail.com>
2021-12-01 20:47:09 -0600

    installed inquirer package

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const pageHTML = generatePage(username, github);
 
-const [username, github] = profileDataArgs;
+// fs.writeFile('./index.html', pageHTML, (err) => {
+//   if (err) throw err;
 
-fs.writeFile('index.html', generatePage(username, github), (err) => {
-  if (err) throw err;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([{ type: 'input', name: 'name', message: 'What is your name?' }])
+  .then((answers) => {
+    console.log(answers);
+  });
